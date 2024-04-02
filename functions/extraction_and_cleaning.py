e8b657a07ffc4f5acabc31f94ca37ff47216562d
tonderai-madamba <tonderaimadamba@ischool.berkeley.edu>
2024-04-02 10:41:09 -0500

    fix reading the files completely stop the collapse

--- functions/extraction_and_cleaning.py
+++ functions/extraction_and_cleaning.py
@@ -1,12 +1,14 @@
+"""This is having some issues when reading the data frame and keeping everything together something wrong"""
 import pandas as pd 
 import glob
 
 def read_my_csv_files(path):
     filenames = glob.glob(path + "/*.csv")
-    return  [pd.read_csv(i) for i in filenames]
+    return  [pd.read_csv(i, encoding='utf-8') for i in filenames]
 
 def combine_df_list(list):
-    return pd.concat(list)
+    return pd.concat(list, ignore_index=True, axis=0, join='outer',)
 
 def read_bronze():
     return  combine_df_list(read_my_csv_files("data/bronze"))
+
