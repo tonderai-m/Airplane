f9e362940b37312161ff9dd3844d8c6695a44b30
tonderai-madamba <tonderaimadamba@ischool.berkeley.edu>
2024-04-02 10:49:03 -0500

    tonderai madamba

--- functions/extraction_and_cleaning.py
+++ functions/extraction_and_cleaning.py
@@ -1,14 +1,16 @@
-"""This is having some issues when reading the data frame and keeping everything together something wrong"""
-import pandas as pd 
+"""Load & Combine DataFrame"""
 import glob
+import pandas as pd
 
 def read_my_csv_files(path):
+    """return pandas list of DataFrames"""
     filenames = glob.glob(path + "/*.csv")
     return  [pd.read_csv(i, encoding='utf-8') for i in filenames]
 
-def combine_df_list(list):
-    return pd.concat(list, ignore_index=True, axis=0, join='outer',)
+def combine_df_list(lis):
+    """return one giant DataFrame"""
+    return pd.concat(lis, ignore_index=True, axis=0, join='outer')
 
 def read_bronze():
+    """return full DataFrame"""
     return  combine_df_list(read_my_csv_files("data/bronze"))
-
